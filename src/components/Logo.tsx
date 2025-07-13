ff82526643780e9a0dbf49eb7aea77d481053d0c
Anastasia <anastasiaerofeeva121@gmail.com>
2025-07-13 16:30:59 -0700

    путь лого

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,9 @@
 import { NavLink } from 'react-router-dom';
-
+import logo from '@/assets/logo.png'
 const Logo = () => {
   return (
     <NavLink className='flex justify-center items-center gap-1.5' to='/'>
-    <img src="/src/assets/logo.png" alt="logo" width='60' />
+    <img src={logo} width='60' />
     <h1 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-300">
           KinoFlow
         </h1>
